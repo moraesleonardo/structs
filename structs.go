73fc8e7a38ed73fb530758048b876fe61ced043a
Leonardo Moraes <leonardo.moraes1@yahoo.com>
2026-06-01 18:03:19 -0300

    Main code

--- structs.go
+++ structs.go
@@ -1,5 +1,22 @@
 package main
 
+import(
+	"fmt"
+)
+
 func main(){
-	print("Structs")
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	//...do something awsesome with that gathered data!
+
+	fmt.Println(firstName, lastName, birthdate)
+}
+
+func getUserData(promptText string) string{
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
 }
