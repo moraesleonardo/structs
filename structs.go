bf15d465b3fb7c0e317e004d6e4d5f442eaf7466
Leonardo Moraes <leonardo.moraes1@yahoo.com>
2026-06-02 15:17:44 -0300

    Package user

--- structs.go
+++ structs.go
@@ -6,35 +6,7 @@ import (
 	"time"
 )
 
-type user struct {
-	firstName string 
-	lastName string
-	birthDate string
-	createdAt time.Time
-}
 
-func (u user) outputUserDetails(){
-
-
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
-}
-
-func (u *user) clearUserName(){
-	u.firstName = ""
-	u.lastName = ""
-}
-
-func newUser(firstName, lastName, birthdate string) (*user, error) {
-	if firstName == "" || lastName == "" || birthdate == "" {
-		return nil, errors.New("First name, last name and birthdate are required.")
-	}
-	return &user{
-		firstName: firstName,
-		lastName: lastName,
-		birthDate: birthdate,
-		createdAt: time.Now(),
-	}, nil
-}
 
 func main(){
 	userFirstName := getUserData("Please enter your first name: ")
---
+++ user/user.go
@@ -0,0 +1,32 @@
+package user
+
+
+type user struct {
+	firstName string 
+	lastName string
+	birthDate string
+	createdAt time.Time
+}
+
+func (u *user) outputUserDetails(){
+
+
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+}
+
+func (u *user) clearUserName(){
+	u.firstName = ""
+	u.lastName = ""
+}
+
+func newUser(firstName, lastName, birthdate string) (*user, error) {
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("First name, last name and birthdate are required.")
+	}
+	return &user{
+		firstName: firstName,
+		lastName: lastName,
+		birthDate: birthdate,
+		createdAt: time.Now(),
+	}, nil
+}
